876697c44888382652ab1ebdcfc93d97b8c3d245
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2022-08-01 19:30:39 +0800

    1，添加说明

--- OpenImageLib/src/main/java/com/flyjingfish/openimagelib/listener/ItemLoadHelper.java
+++ OpenImageLib/src/main/java/com/flyjingfish/openimagelib/listener/ItemLoadHelper.java
@@ -6,5 +6,15 @@ import android.widget.ImageView;
 import com.flyjingfish.openimagelib.beans.OpenImageUrl;
 
 public interface ItemLoadHelper {
+    /**
+     *
+     * @param context
+     * @param openImageUrl 当初传入的其中一个数据
+     * @param imageUrl
+     * @param imageView
+     * @param overrideWidth 图片大小（如果Glide只保存目标大小必须在加载图片时传入）
+     * @param overrideHeight 图片大小（如果Glide只保存目标大小必须在加载图片时传入）
+     * @param onLoadCoverImageListener 回调成功失败
+     */
     void loadImage(Context context, OpenImageUrl openImageUrl, String imageUrl, ImageView imageView, int overrideWidth, int overrideHeight, OnLoadCoverImageListener onLoadCoverImageListener);
 }
