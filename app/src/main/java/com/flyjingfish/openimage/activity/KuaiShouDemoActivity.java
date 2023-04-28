9b610b3468d553118d5a31f7b2462e03236e2a9d
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2023-04-28 19:07:51 +0800

    1，完善快手示例

--- app/src/main/java/com/flyjingfish/openimage/activity/KuaiShouDemoActivity.java
+++ app/src/main/java/com/flyjingfish/openimage/activity/KuaiShouDemoActivity.java
@@ -46,16 +46,9 @@ public class KuaiShouDemoActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityKuaishouDemoBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        setSelect(2);
         binding.rv.rv.setLayoutManager(new GridLayoutManager(this, 2));
         loadData();
 
-    }
-
-    private int layoutType;
-
-    private void setSelect(int pos) {
-        layoutType = pos;
     }
 
     private void loadData() {
@@ -95,39 +88,23 @@ public class KuaiShouDemoActivity extends BaseActivity {
         @NonNull
         @Override
         public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            View view;
-            if (layoutType == 3) {
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_staggered, parent, false);
-            } else {
-                view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_listview, parent, false);
-            }
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_kuaishouw, parent, false);
             return new MyHolder(view);
         }
 
         @Override
         public void onBindViewHolder(@NonNull MyHolder holder, int position) {
-            if (layoutType == 1) {
-                ViewGroup.LayoutParams layoutParams = holder.ivImage.getLayoutParams();
-                layoutParams.width = (int) ScreenUtils.dp2px(KuaiShouDemoActivity.this, 100);
-                layoutParams.height = (int) ScreenUtils.dp2px(KuaiShouDemoActivity.this, 100);
-            } else if (layoutType == 0 || layoutType == 2) {
-                ViewGroup.LayoutParams layoutParams = holder.ivImage.getLayoutParams();
-                layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
-                layoutParams.height = (int) ScreenUtils.dp2px(KuaiShouDemoActivity.this, 220);
-            }
-            if (layoutType == 3) {
-                MyImageLoader.getInstance().load(holder.ivImage, datas.get(position).getCoverImageUrl(), R.mipmap.img_load_placeholder, R.mipmap.img_load_placeholder);
-            } else {
-                MyImageLoader.getInstance().load(holder.ivImage, datas.get(position).getCoverImageUrl(), R.mipmap.img_load_placeholder, R.mipmap.img_load_placeholder);
-
-            }
+            ViewGroup.LayoutParams layoutParams = holder.ivImage.getLayoutParams();
+            layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT;
+            layoutParams.height = (int) ScreenUtils.dp2px(KuaiShouDemoActivity.this, 220);
+            MyImageLoader.getInstance().load(holder.ivImage, datas.get(position).getCoverImageUrl(), R.mipmap.img_load_placeholder, R.mipmap.img_load_placeholder);
             holder.ivImage.setOnClickListener(v -> {
                 OpenImage.with(KuaiShouDemoActivity.this).setClickRecyclerView(binding.rv.rv, new SourceImageViewIdGet() {
-                    @Override
-                    public int getImageViewId(OpenImageUrl data, int position) {
-                        return R.id.iv_image;
-                    }
-                }).setAutoScrollScanPosition(true)
+                            @Override
+                            public int getImageViewId(OpenImageUrl data, int position) {
+                                return R.id.iv_image;
+                            }
+                        }).setAutoScrollScanPosition(true)
                         .setSrcImageViewScaleType(ImageView.ScaleType.CENTER_CROP, true)
                         .setImageUrlList(datas)
                         .addPageTransformer(new ScaleInTransformer())
