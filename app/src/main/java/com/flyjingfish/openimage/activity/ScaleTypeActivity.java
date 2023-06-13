901d531e460ed3b6389d125aece05f94861e9622
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2023-06-13 13:00:44 +0800

    1、优化center和centerInside显示问题

--- app/src/main/java/com/flyjingfish/openimage/activity/ScaleTypeActivity.java
+++ app/src/main/java/com/flyjingfish/openimage/activity/ScaleTypeActivity.java
@@ -29,8 +29,8 @@ public class ScaleTypeActivity extends BaseActivity {
         binding = ActivityScaleTypeBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         itemData = new ImageEntity();
-        itemData.url = "http://pic1.win4000.com/wallpaper/2018-08-15/5b739582b7e78.jpg";
-        itemData.coverUrl = "http://pic1.win4000.com/wallpaper/2018-08-15/5b739582b7e78_270_152.jpg";
+        itemData.url = "https://pics4.baidu.com/feed/50da81cb39dbb6fd95aa0c599b8d0d1e962b3708.jpeg?token=bf17224f51a6f4bb389e787f9c487940";
+        itemData.coverUrl = null;
         setData();
         binding.seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -75,18 +75,21 @@ public class ScaleTypeActivity extends BaseActivity {
                 binding.tvPic2.setBackgroundColor(Color.TRANSPARENT);
                 binding.tvPic3.setBackgroundColor(Color.TRANSPARENT);
                 itemData.url = "https://pics4.baidu.com/feed/50da81cb39dbb6fd95aa0c599b8d0d1e962b3708.jpeg?token=bf17224f51a6f4bb389e787f9c487940";
+                itemData.coverUrl = null;
                 break;
             case R.id.tv_pic2:
                 binding.tvPic2.setBackgroundColor(getResources().getColor(R.color.teal_200));
                 binding.tvPic1.setBackgroundColor(Color.TRANSPARENT);
                 binding.tvPic3.setBackgroundColor(Color.TRANSPARENT);
-                itemData.url = "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fp0.itc.cn%2Fq_70%2Fimages03%2F20210227%2F6687c969b58d486fa2f23d8488b96ae4.jpeg&refer=http%3A%2F%2Fp0.itc.cn&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=auto?sec=1661701773&t=19043990158a1d11c2a334146020e2ce";
+                itemData.url = "https://desk-fd.zol-img.com.cn/t_s960x600c5/g5/M00/02/03/ChMkJ1bKxnyIIvklAAHSVV_HEKsAALHlwPNfqMAAdJt436.jpg";
+                itemData.coverUrl = "https://desk-fd.zol-img.com.cn/t_s432x270c5/g5/M00/02/03/ChMkJ1bKxnyIIvklAAHSVV_HEKsAALHlwPNfqMAAdJt436.jpg";
                 break;
             case R.id.tv_pic3:
                 binding.tvPic3.setBackgroundColor(getResources().getColor(R.color.teal_200));
                 binding.tvPic1.setBackgroundColor(Color.TRANSPARENT);
                 binding.tvPic2.setBackgroundColor(Color.TRANSPARENT);
                 itemData.url = "https://img1.baidu.com/it/u=3124924291,3476865151&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=4841";
+                itemData.coverUrl = null;
                 break;
         }
         setData();
