16c23150a2d96c1920f9aef66db6c07c6837a306
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-09-19 15:29:02 +0800

    1、修复bug

--- OpenImageLib/src/main/java/com/flyjingfish/openimagelib/PhotosViewModel.java
+++ OpenImageLib/src/main/java/com/flyjingfish/openimagelib/PhotosViewModel.java
@@ -4,22 +4,22 @@ import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.MutableLiveData;
 
 import com.flyjingfish.openimagelib.live_data.UnPeekLiveData;
 
-
 public class PhotosViewModel extends AndroidViewModel {
-    UnPeekLiveData<Boolean> closeViewLiveData = new UnPeekLiveData<>();
+    MutableLiveData<Boolean> closeViewLiveData = new MutableLiveData<>();
     public UnPeekLiveData<Boolean> transitionEndLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<Float> onTouchScaleLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<Float> onTouchCloseLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<String> onAddItemListenerLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<String> onAddItemLongListenerLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<String> onRemoveItemListenerLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<String> onRemoveItemLongListenerLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<String> onAddOnSelectMediaListenerLiveData = new UnPeekLiveData<>();
-    UnPeekLiveData<String> onRemoveOnSelectMediaListenerLiveData = new UnPeekLiveData<>();
-    public UnPeekLiveData<Boolean> onCanLayoutLiveData = new UnPeekLiveData<>();
+    MutableLiveData<Float> onTouchScaleLiveData = new MutableLiveData<>();
+    MutableLiveData<Float> onTouchCloseLiveData = new MutableLiveData<>();
+    MutableLiveData<String> onAddItemListenerLiveData = new MutableLiveData<>();
+    MutableLiveData<String> onAddItemLongListenerLiveData = new MutableLiveData<>();
+    MutableLiveData<String> onRemoveItemListenerLiveData = new MutableLiveData<>();
+    MutableLiveData<String> onRemoveItemLongListenerLiveData = new MutableLiveData<>();
+    MutableLiveData<String> onAddOnSelectMediaListenerLiveData = new MutableLiveData<>();
+    MutableLiveData<String> onRemoveOnSelectMediaListenerLiveData = new MutableLiveData<>();
+    public MutableLiveData<Boolean> onCanLayoutLiveData = new MutableLiveData<>();
 
     public PhotosViewModel(@NonNull Application application) {
         super(application);
