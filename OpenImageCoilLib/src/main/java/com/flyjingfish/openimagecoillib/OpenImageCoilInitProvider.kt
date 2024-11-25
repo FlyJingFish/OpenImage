acbe0ce5445b41f768fdc35d18366db9b69c9e5b
tianxiangyu <96164429+FlyJingFish@users.noreply.github.com>
2024-11-25 15:16:22 +0800

    1、完善coil

---
+++ OpenImageCoilLib/src/main/java/com/flyjingfish/openimagecoillib/OpenImageCoilInitProvider.kt
@@ -0,0 +1,70 @@
+package com.flyjingfish.openimagecoillib
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.database.Cursor
+import android.net.Uri
+import coil.Coil
+import com.flyjingfish.openimagelib.OpenImageConfig
+import com.flyjingfish.openimagelib.utils.OpenImageLogUtils
+
+class OpenImageCoilInitProvider : ContentProvider() {
+    override fun onCreate(): Boolean {
+        OpenImageLogUtils.init(context!!.applicationContext)
+        var isCoil3 = false
+        try {
+            val clazz: Class<*> = Coil::class.java
+            clazz.toString()
+        } catch (e: NoClassDefFoundError) {
+            isCoil3 = true
+        }
+        //初始化大图加载器
+        if (OpenImageConfig.getInstance().bigImageHelper == null) {
+            if (isCoil3){
+                OpenImageConfig.getInstance().bigImageHelper = Coil3BigImageHelper()
+            }else{
+                OpenImageConfig.getInstance().bigImageHelper = CoilBigImageHelper()
+            }
+        }
+        //初始化下载原图或视频类
+        if (OpenImageConfig.getInstance().downloadMediaHelper == null) {
+            if (isCoil3){
+                OpenImageConfig.getInstance().downloadMediaHelper = Coil3DownloadMediaHelper()
+            }else{
+                OpenImageConfig.getInstance().downloadMediaHelper = CoilDownloadMediaHelper()
+            }
+        }
+        return true
+    }
+
+    override fun query(
+        uri: Uri,
+        projection: Array<String>?,
+        selection: String?,
+        selectionArgs: Array<String>?,
+        sortOrder: String?
+    ): Cursor? {
+        return null
+    }
+
+    override fun getType(uri: Uri): String? {
+        return null
+    }
+
+    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<String>?): Int {
+        return 0
+    }
+
+    override fun update(
+        uri: Uri,
+        values: ContentValues?,
+        selection: String?,
+        selectionArgs: Array<String>?
+    ): Int {
+        return 0
+    }
+}
